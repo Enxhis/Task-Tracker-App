e72d044079d739c9d1f47ad23608b800a7ef8fe5
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-01-21 11:54:39 -0500

    added delete functionality to the app

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,9 +1,39 @@
-function App() {
+import { useState } from 'react'
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: "Feb 15th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at school',
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+    }
+  ]);
+
+  // delete task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
 
   return (
     <div className="container">
-      <h1>Hello From React</h1>
+      <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No tasks to Show'}
 
     </div>
   );
